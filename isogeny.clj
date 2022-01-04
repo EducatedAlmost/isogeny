ebc2ae5a61e5ff46fd08884ad25ddd4cd08de9e1
AlmostEducated <EducatedAlmost@gmail.com>
2022-01-04 18:42:04 +0000

    Isogeny V2.0 rewrite
    Improved logging
    if -> when
    global *verbose?*

--- isogeny.clj
+++ isogeny.clj
@@ -1,12 +1,18 @@
 #!/usr/bin/env bb
+;; -*- mode: clojure -*-
 
 (ns ae.isogeny
-  (:require [clojure.tools.cli :refer [parse-opts]]
-            [babashka.classpath :refer [add-classpath]]
-            [clojure.pprint :as pp]))
+  (:require [clojure.pprint :as pp]
+            [clojure.tools.cli :as cli]
+            [clojure.tools.logging :as log]
+            [selmer.parser :as s.parser]
+            [selmer.util :as s.util]))
 
 (def help-explanation
-  "USAGE: isogeny -t TEMPLATE -c CONTEXT -o OUTPUT")
+  "USAGE: isogeny -t TEMPLATE -c CONTEXT -o OUTPUT
+TEMPLATE: Selmer template
+CONTEXT: EDN context map
+OUTPUT: output location")
 
 (def cli-options
   [["-t" "--template TEMPLATE" "Template" :default *in*]
@@ -14,83 +20,95 @@
    ["-C" "--context-string CONTEXT_STRING" "EDN context string"]
    ["-d" "--context-default CONTEXT_DEFAULT" "Default EDN context"]
    ["-o" "--output OUTPUT" "Output to file"]
-   ["-a" "--add-tags ADD-TAGS" "Provide a file of additional tag definitions"]
-   ["-s" "--strict" "STRICT" "Missing values throw exceptions"]
-   ["-v" "--verbose" "Verbose output"]
-   ["-h" "--help"]])
+   ["-a" "--add-tags ADD_TAGS" "Provide a file of additional tag definitions"]
+   ["-s" "--strict" "Missing values throw exceptions" :id :strict?]
+   ["-v" "--verbose" "Verbose output" :id :verbose?]
+   ["-V" "--version" "Version"]
+   ["-h" "--help" "Display help" :id :help?]])
+
+(def *verbose?* (atom false))
 
 ;; Add the {% env %} tag to read environment variables.
 (selmer.parser/add-tag!
  :env (fn [args _] (System/getenv (first args))))
 
 (defn throw-on-missing
-  "If --strict is used, this sets Selmer to throw on missing values."
+  "Causes an exception to be thrown when a missing value is encountered."
   []
-  (selmer.util/set-missing-value-formatter!
-   (fn [tag context=map]
+  (when @*verbose?* (log/info "Throwing on missing values."))
+  (s.util/set-missing-value-formatter!
+   (fn [tag _]
      (-> "Missing value: "
          (str (or (:tag-value tag) (:tag-name tag)))
          Exception.
          throw))))
 
-(defn read-context
-  "Read the context, falling back on the default context if it can't be found."
-  [options]
-  (let [context-path (if (= "-" (options :context)) *in* (options :context))
-        context-default (options :context-default)]
-    (try (-> context-path slurp read-string)
-         (catch java.io.FileNotFoundException e
-           (if (options :verbose) (println "Using default context: " context-default))
-           (-> context-default slurp read-string)))))
+(defn add-tags! "Add additional tags to use when rendering the template."
+  [add-tags]
+  (when @*verbose?* (log/info "Loading tags from: " add-tags))
+  (try (load-file add-tags)
+       (catch Exception e
+         (log/error "ERROR - Exception thrown loading additional tags:")
+         (throw e))))
 
-(defn ->context
-  "Create the context to be used."
-  [options]
-  (let [context (read-context options)
-        context-string (some-> options :context-string read-string)]
-    (eval (merge context context-string))))
+(defn read-context "Read the context, from a default file if it fails."
+  [context context-default]
+  (try (-> context slurp read-string)
+       (catch Exception e
+         (when @*verbose?* (log/info "Falling back on default context: " context-default))
+         (try (-> context-default slurp read-string)
+              (catch Exception e2
+                (log/error "Neither the context nor its default can be found:" (.getMessage e))
+                (throw e2))))))
+
+(defn ->context "Create the context to be used to render."
+  [context context-default context-string]
+  (when @*verbose?* (log/info "Attempting to read context: " context))
+  (let [context (if (= "-" context) *in* context)
+        context-override (some-> context-string read-string)]
+    (-> (read-context context context-default)
+        (merge context-override)
+        eval)))
 
 (defn ->template
   "Read the template, either from file or STDIN."
-  [options]
-  (-> (or (options :template) *in*) slurp))
+  [template]
+  (let [tmpl (or (if (= template "-") *in* nil) template *in*)]
+    (when @*verbose?* (log/info "Reading template: " (if (= tmpl *in*) "STDIN" tmpl)))
+    (try (slurp tmpl)
+         (catch Exception e
+           (log/error "Exception reading template: " template)
+           (throw e)))))
 
-(defn add-tags!
-  "Add additional tags for the rendering of the template."
-  [options]
-  (try (some-> options :add-tags slurp read-string eval)
-       (catch clojure.lang.ExceptionInfo e
-         (println "Exception thrown loading additional tags:")
-         (throw e))))
-
-(defn render
-  "Render the template in the context."
+(defn render "Render the template using the context."
   [template context]
-  (try (selmer.parser/render template context)
-       (catch clojure.lang.ExceptionInfo e
-         (println "Selmer rendering exception thrown:")
+  (try (s.parser/render template context)
+       (catch Exception e
+         (log/error "Rendering exception thrown:")
          (throw e))))
 
-(defn write
-  "Write the output either to file or STDOUT."
-  [out options]
-  (if-let [output (options :output)]
-    (spit output out)
-    (print out)))
+(defn write "Write content to output or STDOUT."
+  [content output]
+  (when @*verbose?* (log/info "Writing output to: " (or output "STDOUT"))
+        (when-not output (log/info "Output: \n----------------")))
+  (try (if output (spit output content) (print content))
+       (catch Exception e
+         (log/error "ERROR - Cannot write to: " output)
+         (throw e))))
 
-(defn -main []
-  (let [{{help :help verbose? :verbose strict? :strict :as options} :options
-         args :arguments} (parse-opts *command-line-args* cli-options)]
-    (if verbose? (println "Options: " (with-out-str (pp/pprint options))))
-    (if strict? (throw-on-missing))
-    (if help (pp/pprint cli-options)
-        (let [context (->context options)
-              template (->template options)]
-          (add-tags! options)
-          (if verbose? (println "Known variables: " (selmer.parser/known-variables template) "\n"))
-          (println "Output:")
-          (-> template
-              (render context)
-              (write options))))))
+(defn -main [args]
+  (let [{{:keys [template context context-string context-default
+                 output add-tags strict? verbose? version help?]
+          :as options} :options
+         :keys [arguments summary errors]} (cli/parse-opts args cli-options)]
+    (when help? (println help-explanation) (println summary) (System/exit 0))
+    (when version (println "Isogeny 2.0") (System/exit 0))
+    (when strict? (throw-on-missing))
+    (when verbose? (swap! *verbose?* (constantly true)) (log/info "Options: " options))
+    (when add-tags (add-tags! add-tags))
+    (let [ctxt (->context context context-default context-string)
+          tmpl (->template template)]
+      (when @*verbose?* (log/info "Known variables: " (s.parser/known-variables tmpl)))
+      (-> tmpl (render ctxt) (write output)))))
 
-(-main)
+(-main *command-line-args*)
