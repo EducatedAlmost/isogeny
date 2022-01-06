ff1590a1b9f1707aef78d91335b676fed86954c7
AlmostEducated <EducatedAlmost@gmail.com>
2022-01-06 18:22:42 +0000

    Add --prepare and --safe options.

--- isogeny.clj
+++ isogeny.clj
@@ -3,11 +3,12 @@
 
 (ns ae.isogeny
   (:require [babashka.fs :as fs]
-            [clojure.pprint :as pp]
+            [clojure.java.shell :as sh]
             [clojure.tools.cli :as cli]
             [clojure.tools.logging :as log]
             [selmer.parser :as s.parser]
-            [selmer.util :as s.util]))
+            [selmer.util :as s.util]
+            [clojure.string :as str]))
 
 (def usage
   "USAGE: isogeny -t TEMPLATE -c CONTEXT -o OUTPUT
@@ -26,10 +27,26 @@ OUTPUT: output location")
    [nil "--deep-merge" "Recursively merge the CONTEXT and CONTEXT_STRING" :id :deep-merge?]
    ["-s" "--strict" "Missing values throw exceptions" :id :strict?]
    ["-v" "--verbose" "Verbose output" :id :verbose?]
+   [nil "--prepare PREPARE" "Prepare a config file for use with Isogeny"]
+   [nil "--safe" "Will not overwrite an existing file." :id :safe? :default false]
    ["-V" "--version" "Version"]
    ["-h" "--help" "Display help" :id :help?]])
 
 (def *verbose?* (atom false))
+
+(defn prepare-template
+  ([file] (prepare-template file false))
+  ([file safe?]
+   (let [template (str file ".template")
+         host (or (.. java.net.InetAddress getLocalHost getHostName) "HOST")
+         context (str/join "." [file host "edn"])]
+     (try (fs/copy file template {:replace-existing (not safe?)})
+          (catch java.nio.file.FileAlreadyExistsException e
+            (when @*verbose?* (log/warn "Template already exists: " (.getMessage e)))))
+     (try (fs/create-file context)
+          (spit context "{:example \"value\"}\n")
+          (catch java.nio.file.FileAlreadyExistsException e
+            (when @*verbose?* (log/warn "Config already exists: " (.getMessage e))))))))
 
 ;; Add the {% env %} tag to read environment variables.
 (selmer.parser/add-tag!
@@ -106,18 +123,22 @@ OUTPUT: output location")
          (throw e))))
 
 (defn write "Write content to output or STDOUT."
-  [content output]
+  [content output safe?]
   (when @*verbose?* (log/info "Writing output to: " (or output "STDOUT"))
         (when-not output (log/info "Output: \n----------------")))
+  (when (and safe? (fs/exists? output))
+    (throw (new Exception (str "Running safely and output file already exists: " output))))
   (try (if output (spit output content) (print content))
        (catch Exception e
          (log/error "ERROR - Cannot write to: " output)
          (throw e))))
 
-(defn render-one [ctxt template output]
-  (let [tmpl (->template template)]
-    (when @*verbose?* (log/info "Known variables: " (s.parser/known-variables tmpl)))
-    (-> tmpl (render ctxt) (write output))))
+(defn render-one
+  ([ctxt template output] (render-one ctxt template output false))
+  ([ctxt template output safe?]
+   (let [tmpl (->template template)]
+     (when @*verbose?* (log/info "Known variables: " (s.parser/known-variables tmpl)))
+     (-> tmpl (render ctxt) (write output safe?)))))
 
 (defn render-many
   [ctxt templates]
@@ -127,18 +148,19 @@ OUTPUT: output location")
 
 (defn -main [args]
   (let [{{:keys [template multi-template context context-string context-default
-                 output add-tags strict? verbose? version help? deep-merge?]
+                 output add-tags strict? verbose? prepare version help? deep-merge? safe?]
           :as options} :options
          :keys [arguments summary errors]} (cli/parse-opts args cli-options)]
-    (when errors (log/error "Error parsing options and args:" errors))
     (when help? (println usage) (println summary) (System/exit 0))
-    (when version (println "Isogeny 2.0") (System/exit 0))
-    (when strict? (throw-on-missing))
+    (when errors (log/error "Error parsing options and args:" errors))
     (when verbose? (swap! *verbose?* (constantly true)) (log/info "Options: " options))
+    (when version (println "Isogeny 2.0") (System/exit 0))
+    (when prepare (prepare-template prepare safe?) (System/exit 0))
+    (when strict? (throw-on-missing))
     (when add-tags (add-tags! add-tags))
     (let [ctxt (->context context context-default context-string deep-merge?)]
       (if multi-template
         (render-many ctxt arguments)
-        (render-one ctxt template output)))))
+        (render-one ctxt template output safe?)))))
 
 (-main *command-line-args*)
